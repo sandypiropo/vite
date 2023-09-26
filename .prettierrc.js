7ead69c6dd83887fe3e226b21e3fcdcfdd02a2ec
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-09-26 02:50:53 -0300

    att

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    singleQuote: true, // Aspas simples em vez de duplas
+    semi: true,       // Ponto e vírgula no final das linhas
+    trailingComma: 'all', // Vírgula ao final de objetos e arrays
+    printWidth: 80,    // Limite de largura da linha
+  };
+  
